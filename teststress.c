6645a0799e63ce23adeed9236f6c9431d4988534
Kevvy <25ktran8@gmail.com>
2026-04-21 05:48:56 -0400

    new .c

--- teststress.c
+++ teststress.c
@@ -6,17 +6,26 @@ int main(void)
 {
     int i;
     int iChar;
+    int iLineCount = 0;
+    int iCharCount = 0;
 
     srand((unsigned int)time(NULL));
 
-    for (i = 0; i < 500000; i++)
+    for (i = 0; i < 500000 && iCharCount < 50000; i++)
     {
         iChar = rand() % 0x7F;
 
-        if (iChar == 0x09 || iChar == 0x0A ||
+        if (iChar == 0x09 ||
             (iChar >= 0x20 && iChar <= 0x7E))
         {
             putchar(iChar);
+            iCharCount++;
+        }
+        else if (iChar == 0x0A && iLineCount < 999)
+        {
+            putchar(iChar);
+            iCharCount++;
+            iLineCount++;
         }
     }
 
